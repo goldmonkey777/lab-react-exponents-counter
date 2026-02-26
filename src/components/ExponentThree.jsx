6d65057413173103549b58c9c5166de592a770a8
goldmonkey777 <contact@goldmonkey.studio>
2026-02-26 13:37:26 +0100

    Refactor ExponentThree to accept num as a prop

--- src/components/ExponentThree.jsx
+++ src/components/ExponentThree.jsx
@@ -1,7 +1,7 @@
-const ExponentThree = () => (
-   <div className="exponent-counter-container">
+const ExponentThree = ({ num }) => (
+  <div className="exponent-counter-container">
     <p className="exponent-label">n³</p>
-    <p className="exponent-result">2 * 2 * 2 = <span className="total">8</span></p>
+    <p className="exponent-result">{num} * {num} * {num} = <span className="total">{num ** 3}</span></p>
   </div>
 );
 
