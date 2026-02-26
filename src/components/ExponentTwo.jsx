5b57e7660665523b1a8ed9b596ddd5d2433dbc99
goldmonkey777 <contact@goldmonkey.studio>
2026-02-26 13:36:10 +0100

    done

--- src/components/ExponentTwo.jsx
+++ src/components/ExponentTwo.jsx
@@ -1,7 +1,7 @@
-const ExponentTwo = () => (
-   <div className="exponent-counter-container">
+const ExponentTwo = ({ num }) => (
+  <div className="exponent-counter-container">
     <p className="exponent-label">n²</p>
-    <p className="exponent-result">2 * 2 = <span className="total">4</span></p>
+    <p className="exponent-result">{num} * {num} = <span className="total">{num ** 2}</span></p>
   </div>
 );
 
