5f8c3b56008bd41685893e1a8331e6a33a5ff306
goldmonkey777 <contact@goldmonkey.studio>
2026-02-26 13:40:59 +0100

    Refactor Exponent components into a reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import Counter from "./components/Counter";
-import ExponentTwo from "./components/ExponentTwo";
-import ExponentThree from "./components/ExponentThree";
-import ExponentFour from "./components/ExponentFour";
-import ExponentFive from "./components/ExponentFive";
-import ExponentSix from "./components/ExponentSix";
+import Exponent from "./components/Exponent";
 
 function App () {
   // Iteration 1: Lift state up from Counter
@@ -26,12 +22,12 @@ function App () {
       <h2><em>Exponents</em></h2>
 
       <div className="container">
-        {/* Iteration 2: Pass count as prop to all Exponent components */}
-        <ExponentTwo num={count} />
-        <ExponentThree num={count} />
-        <ExponentFour num={count} />
-        <ExponentFive num={count} />
-        <ExponentSix num={count} />
+        {/* Iteration 4: Use reusable Exponent component */}
+        <Exponent num={count} exponent={2} />
+        <Exponent num={count} exponent={3} />
+        <Exponent num={count} exponent={4} />
+        <Exponent num={count} exponent={5} />
+        <Exponent num={count} exponent={6} />
       </div>
     </div>
   );
